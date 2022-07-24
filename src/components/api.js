f86cc0688719fa6730c6009a72610d61dab2c2e1
kvksatish <kvksatish98@gmail.com>
2022-07-24 11:19:17 +0530

    all done api

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export function headlinesresult() {
     return (
-        axios.get(`https://gnews.io/api/v4/top-headlines?token=6dbc1b5af981f1f87ac57bd830cfd56e
+        axios.get(`https://gnews.io/api/v4/top-headlines?token=4568829edcf01ad16258a6b01146b7a5
 
 &country=in&lang=en`).then((res) => {
 
@@ -12,10 +12,11 @@ export function headlinesresult() {
 
 
 export function newsresult(search) {
-    return (axios.get((`https://gnews.io/api/v4/search?q=${search}&token=6dbc1b5af981f1f87ac57bd830cfd56e
+    return (axios.get((`https://gnews.io/api/v4/search?q=${search}&token=4568829edcf01ad16258a6b01146b7a5
 
 &country=in`)).then((res) => {
         return res.data
 
     }).catch((err) => console.log(err)))
 }
+//6dbc1b5af981f1f87ac57bd830cfd56e
